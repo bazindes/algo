3b6577a593051292a2d3a88d4f2c3bdd458afc91
baz <wanghe0824@gmail.com>
2019-03-29 18:10:52 -0400

    add prac && invertlinkedlist

--- main.cpp
+++ main.cpp
@@ -4,16 +4,22 @@
     Date: 2019-03-28 21:10
 */
 
-#include "ds/list/LinkedList.h"
 #include <iostream>
+#include "ds/list/LinkedList.h"
+
+#include "prac/InvertLinkedList.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
   int nums[] = {1, 2, 3, 5};
   LinkedList list(4, nums);
-
   list.printLinkedList();
+  cout << " ----- \n";
+  invert(&list);
+  cout << " ----- \n";
+  list.printLinkedList();
+
   return 0;
 }
 //
---
+++ prac/InvertLinkedList.cpp
@@ -0,0 +1,31 @@
+/*
+  InvertLinkedList.cpp
+  Created by: baz
+  Date: 2019-03-29 17:10
+*/
+#include "../ds/list/LinkedList.h"
+
+// static LinkedListNode *helper(LinkedListNode *node) {
+//   if (!node->next)
+//     return node;
+//   LinkedListNode *cur = node;
+//   LinkedListNode *prev = helper(node->next);
+//   prev->next = cur;
+//   return cur;
+// }
+
+LinkedList *invert(LinkedList *list) {
+  LinkedListNode *cur = list->head;
+  LinkedListNode *n;
+  LinkedListNode *prev;
+
+  while(cur){
+    n = cur -> next;
+    cur -> next = prev;
+    prev = cur;
+    cur = n;
+  }
+
+  list -> head = prev;
+  return list;
+}
