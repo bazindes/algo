a19e7f5544e80520f82980770746de2c0d84e7b6
baz <wanghe0824@gmail.com>
2019-04-02 21:03:31 -0400

    update 204

--- lc/easy/CountPrimes_204.cpp
+++ lc/easy/CountPrimes_204.cpp
@@ -3,7 +3,23 @@
   Created by: baz
   Date: 2019-04-02 20:40
 */
+#include <cmath>
+#include <vector>
+using namespace std;
 
 int countPrimes(int n) {
-  
+  vector<int> seive(n + 1, 1);
+
+  for (int i = 2; i <= sqrt(n); i++) {
+    if (seive[i] == 1) {
+      for (int j = i * i; j <= n; j = j + i) {
+        seive[j] = 0;
+      }
+    }
+  }
+  int cnt = 0;
+  for (int i = 2; i < n; i++)
+    if (seive[i])
+      cnt++;
+  return cnt;
 }
