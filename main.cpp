c67723777fb9b0dad671c1ba26e79dd6674f7d1a
baz <wanghe0824@gmail.com>
2019-04-03 21:00:48 -0400

    add 256

--- main.cpp
+++ main.cpp
@@ -5,30 +5,36 @@
 */
 
 #include <iostream>
-#include "ds/list/LinkedList.h"
+#include <vector>
+// #include "ds/list/LinkedList.h"
 // #include "prac/InvertLinkedList.cpp"
 // #include "lc/easy/TwoSum3DatastructureDesign_170.cpp"
-#include "lc/easy/RemoveLinkedListElements_203.cpp"
+// #include "lc/easy/RemoveLinkedListElements_203.cpp"
+#include "lc/easy/PaintHouse_256.cpp"
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 
-  int nums[] = {1,2,2,1};
-  LinkedList list(4, nums);
-  list.printLinkedList();
-  list.head = removeElements(list.head, 2);
-  list.printLinkedList();
-  int nums2[] = {1,2,6,3,4,5,6};
-  LinkedList list2(7, nums2);
-  list2.printLinkedList();
-  list2.head = removeElements(list2.head, 6);
-  list2.printLinkedList();
-  int nums3[] = {1,1,1};
-  LinkedList list3(3, nums3);
-  list3.printLinkedList();
-  list3.head = removeElements(list3.head, 1);
-  list3.printLinkedList();
+  vector<vector<int>> costs = {{17,2,17},{16,16,5},{14,3,19}};
+  costs = {{3,5,3},{6,17,6},{7,13,18},{9,10,18}};
+  cout << minCost(costs) << endl;
+
+  // int nums[] = {1,2,2,1};
+  // LinkedList list(4, nums);
+  // list.printLinkedList();
+  // list.head = removeElements(list.head, 2);
+  // list.printLinkedList();
+  // int nums2[] = {1,2,6,3,4,5,6};
+  // LinkedList list2(7, nums2);
+  // list2.printLinkedList();
+  // list2.head = removeElements(list2.head, 6);
+  // list2.printLinkedList();
+  // int nums3[] = {1,1,1};
+  // LinkedList list3(3, nums3);
+  // list3.printLinkedList();
+  // list3.head = removeElements(list3.head, 1);
+  // list3.printLinkedList();
 
 
   // int nums[] = {1, 2, 3, 5};
