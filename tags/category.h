6ceb27ccf8d0db71991ec1ae275449fd5ca7b5c1
baz <wanghe0824@gmail.com>
2019-04-10 22:05:24 -0400

    add tags

---
+++ tags/category.h
@@ -0,0 +1,46 @@
+/*
+  category.h
+  Created by: baz
+  Date: 2019-04-10 21:51
+*/
+namespace category {
+  
+struct Array {};
+struct DP {};
+struct Math {};
+struct String {};
+struct Tree {};
+struct HashTable {};
+struct DFS {};
+struct BFS {};
+struct BinarySearch {};
+struct TwoPointers {};
+struct Greedy {};
+struct Stack {};
+struct Backtracking {};
+struct Design {};
+struct LinkedList {};
+struct BitManipulation {};
+struct Heap {};
+struct Graph {};
+struct Sort {};
+struct UnionFind {};
+struct DC {};
+struct BST {};
+struct Trie {};
+struct SlidingWindow {};
+struct Recursion {};
+struct Queue {};
+struct SegmentTree {};
+struct Minimax {};
+struct Random {};
+struct BinaryIndexedTree {};
+struct TopologicalSort {};
+struct Brainteaser {};
+struct Geometry {};
+struct Map {};
+struct RejectionSampling {};
+struct ReservoirSampling {};
+struct Memoization {};
+
+} // namespace category
---
+++ tags/company.h
@@ -0,0 +1,46 @@
+/*
+  company.h
+  Created by: baz
+  Date: 2019-04-10 21:59
+*/
+
+namespace company {
+struct Google {};
+struct Amazon {};
+struct Facebook {};
+struct Microsoft {};
+struct Bloomberg {};
+struct Uber {};
+struct Apple {};
+struct Adobe {};
+struct LinkedIn {};
+struct Yahoo {};
+struct Airbnb {};
+struct Snapchat {};
+struct Twitter {};
+struct GoldmanSachs {};
+struct Oracle {};
+struct eBay {};
+struct Yelp {};
+struct VMware {};
+struct Lyft {};
+struct Expedia {};
+struct Cisco {};
+struct Paypal {};
+struct Zenefits {};
+struct Zillow {};
+struct Pinterest {};
+struct PocketGems {};
+struct Intuit {};
+struct Indeed {};
+struct Mathworks {};
+struct Nutanix {};
+struct Nvidia {};
+struct TwoSigma {};
+struct Hulu {};
+struct PalantirTech {};
+struct Dropbox {};
+struct TripAdvisor {};
+struct Wish {};
+
+} // namespace company
