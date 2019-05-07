e24f128eea86fc93f83e0716e6114ee73fa48e64
baz <wanghe0824@gmail.com>
2019-05-07 18:44:35 -0400

    remove cmake

--- src/lc/easy/CountAndSay_38.cpp
+++ src/lc/easy/CountAndSay_38.cpp
@@ -1,4 +1,5 @@
-// The count-and-say sequence is the sequence of integers with the first five terms as following:
+// The count-and-say sequence is the sequence of integers with the first five
+// terms as following:
 
 // 1.     1
 // 2.     11
@@ -9,7 +10,8 @@
 // 11 is read off as "two 1s" or 21.
 // 21 is read off as "one 2, then one 1" or 1211.
 
-// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the count-and-say sequence.
+// Given an integer n where 1 ≤ n ≤ 30, generate the nth term of the
+// count-and-say sequence.
 
 // Note: Each term of the sequence of integers will be represented as a string.
 
@@ -27,75 +29,63 @@
 
 using namespace std;
 
-class CountAndSay_38
-{
-  private:
-    string helper(string s)
-    {
-        string ans = "";
-        int k = 0;
-        for (int i = 0; i < s.size(); i++)
-        {
-            if (s[i] != s[k])
-            {
-                ans += to_string(i - k);
-                ans += s[k];
-                k = i;
-            }
-            if (i == s.size() - 1)
-            {
-                ans += to_string(i - k + 1);
-                ans += s[k];
-            }
-        }
-        return ans;
+class CountAndSay_38 {
+private:
+  string helper(string s) {
+    string ans = "";
+    int k = 0;
+    for (int i = 0; i < s.size(); i++) {
+      if (s[i] != s[k]) {
+        ans += to_string(i - k);
+        ans += s[k];
+        k = i;
+      }
+      if (i == s.size() - 1) {
+        ans += to_string(i - k + 1);
+        ans += s[k];
+      }
     }
+    return ans;
+  }
 
-  public:
-    string countAndSay(int n);
-    string countAndSay_(int n);
-    void test();
+public:
+  string countAndSay(int n);
+  string countAndSay_(int n);
+  void test();
 };
 
-string CountAndSay_38::countAndSay(int n)
-{
-    if (n == 1)
-    {
-        return "1";
-    }
-    return helper(countAndSay(n - 1));
+string CountAndSay_38::countAndSay(int n) {
+  if (n == 1) {
+    return "1";
+  }
+  return helper(countAndSay(n - 1));
 }
 
-//O(n) O(1)
-string CountAndSay_38::countAndSay_(int n)
-{
-    string res = "1", cur = "1";
-    while (--n)
-    {
-        char ch = res[0];
-        int count = 0;
-        cur = "";
-        for (int i = 0; i < res.size(); i++)
-        {
-            if (ch == res[i])
-                count++;
-            else
-            {
-                cur += count + '0';
-                cur += ch;
-                ch = res[i];
-                count = 1;
-            }
-        }
+// O(n) O(1)
+string CountAndSay_38::countAndSay_(int n) {
+  string res = "1", cur = "1";
+  while (--n) {
+    char ch = res[0];
+    int count = 0;
+    cur = "";
+    for (int i = 0; i < res.size(); i++) {
+      if (ch == res[i])
+        count++;
+      else {
         cur += count + '0';
         cur += ch;
-        res = cur;
+        ch = res[i];
+        count = 1;
+      }
     }
-    return res;
+    cur += count + '0';
+    cur += ch;
+    res = cur;
+  }
+  return res;
 }
 
-void CountAndSay_38::test()
-{
-    cout << countAndSay(1) << endl;
-    cout << countAndSay(4) << endl;
+void CountAndSay_38::test() {
+  cout << countAndSay(1) << endl;
+  cout << countAndSay(4) << endl;
 }
