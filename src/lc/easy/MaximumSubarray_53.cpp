62a280c57ce57b4676dc3b5cbf7401414a296fe7
baz <wanghe0824@gmail.com>
2019-05-13 20:57:57 -0400

    add 53

--- src/lc/easy/MaximumSubarray_53.cpp
+++ src/lc/easy/MaximumSubarray_53.cpp
@@ -5,18 +5,17 @@
 */
 
 /**
- * 
-Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-Example:
-Input: [-2,1,-3,4,-1,2,1,-5,4],
-Output: 6
-Explanation: [4,-1,2,1] has the largest sum = 6.
-Follow up:
+ *
+Given an integer array nums, find the contiguous subarray (containing at least
+one number) which has the largest sum and return its sum. Example: Input:
+[-2,1,-3,4,-1,2,1,-5,4], Output: 6 Explanation: [4,-1,2,1] has the largest sum
+= 6. Follow up:
 
-If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+If you have figured out the O(n) solution, try coding another solution using the
+divide and conquer approach, which is more subtle.
 */
 
-#include "../util/pch.h"
+#include "../../util/pch.h"
 
 using namespace std;
 
@@ -31,4 +30,9 @@ int maxSubArray(vector<int> &nums) {
     maxs = maxs < p ? p : maxs;
   }
   return maxs;
+}
+
+int main(void) {
+  vector<int> vc = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+  cout << maxSubArray(vc) << endl;
 }
