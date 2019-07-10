9227c84c54a4fd7f94113d0ae2b88f316cd9bf47
baz <wanghe0824@gmail.com>
2019-07-10 18:50:44 -0400

    add 119

---
+++ src/lc/easy/PascalsTriangleII_119.cpp
@@ -0,0 +1,46 @@
+/*
+  PascalsTriangle_118.cpp
+  Created by: baz
+  Date: 2019-07-10 18:17
+*/
+
+/**
+ Given a non-negative index k where k ≤ 33, return the kth index row of the
+Pascal's triangle.
+
+Note that the row index starts from 0.
+
+
+In Pascal's triangle, each number is the sum of the two numbers directly above
+it.
+
+Example:
+
+Input: 3
+Output: [1,3,3,1]
+Follow up:
+
+Could you optimize your algorithm to use only O(k) extra space?
+ */
+
+#include "../../util/pch.h"
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<int> getRow(int rowIndex) {
+  vector<int> ans(rowIndex + 1);
+  ans[0] = 1;
+  for(int i= 0; i <= rowIndex; i++){
+    for(int j = i; j > 0; j --){
+      ans[j] = ans[j] + ans[j - 1];
+    }
+  }
+  return ans;
+}
+
+int main() {
+  cout << getRow(5) << endl;
+  return 0;
+}
